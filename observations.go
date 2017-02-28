c9b76a7e11a28f28f57db537e168dca0a3a18234
Michael Camilleri <mike@mikecamilleri.com>
2017-02-28 12:53:42 -0800

    complete observations implementation with tests

--- observations.go
+++ observations.go
@@ -1,49 +1,79 @@
 package nws
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
+
+	"golang.org/x/net/html/charset"
 )
 
 const (
+	observationTimeFmt        = time.RFC1123Z
 	currentObservationsURLFmt = "http://w1.weather.gov/xml/current_obs/%s.xml"
 )
 
-// Observation holds a single meteorological observation. The fields and XML
-// mappings are based on http://www.nws.noaa.gov/view/current_observation.xsd.
-// See http://w1.weather.gov/xml/current_obs/ for general information. Numeric
-// fields are type `string` so that empty strings may represent missing data in
-// the struct. Some fields in the XSD are not unmarshalled because they are
-// unecessary for the purposes of this package.
+// Observation holds a single meteorological observation. Numeric fields are
+// type `string` so that empty strings may represent missing data in the struct
+// and so that decimal values remain exact.
 type Observation struct {
-	SuggestedPickup       string `xml:"suggested_pickup"`
-	SuggestedPickupPeriod string `xml:"suggested_pickup_period"`
+	Location         string `xml:"location"`
+	StationId        string `xml:"station_id"`
+	Latitude         string `xml:"latitude"`
+	Longitude        string `xml:"longitude"`
+	Elevation        string `xml:"elevation"`
+	Time             time.Time
+	Weather          string `xml:"weather"`
+	TempF            string `xml:"temp_f"`
+	TempC            string `xml:"temp_c"`
+	RelativeHumidity string `xml:"relative_humidity"`
+	WindDir          string `xml:"wind_dir"`
+	WindDegrees      string `xml:"wind_degrees"`
+	WindMph          string `xml:"wind_mph"`
+	WindKt           string `xml:"wind_kt"`
+	WindGustMph      string `xml:"wind_gust_mph"`
+	WindGustKt       string `xml:"wind_gust_kt"`
+	PressureMb       string `xml:"pressure_mb"`
+	PressureIn       string `xml:"pressure_in"`
+	DewpointF        string `xml:"dewpoint_f"`
+	DewpointC        string `xml:"dewpoint_c"`
+	HeatIndexF       string `xml:"heat_index_f"`
+	HeatIndexC       string `xml:"heat_index_c"`
+	WindchillF       string `xml:"windchill_f"`
+	WindchillC       string `xml:"windchill_c"`
+	VisibilityMi     string `xml:"visibility_mi"`
+}
+
+// observation is a private struct use to export a single meteorological
+// observation into. The fields and XML mappings are based on
+// http://www.nws.noaa.gov/view/current_observation.xsd. This package is built
+// with an eye towards home automation applications and usefullness for most
+// people. Some available fields are not included becuase they are outside the
+// scope of this package.
+type observation struct {
+	// SuggestedPickup       string `xml:"suggested_pickup"`
+	// SuggestedPickupPeriod string `xml:"suggested_pickup_period"`
 	Location              string `xml:"location"`
 	StationId             string `xml:"station_id"`
-	Latitude              string `xml:"longitude"`
-	Longitude             string `xml:"latitude"`
+	Latitude              string `xml:"latitude"`
+	Longitude             string `xml:"longitude"`
 	Elevation             string `xml:"elevation"`
-	observationTimeRFC822 string `xml:"observation_time_rfc822"`
-	Time                  time.Time
+	ObservationTimeRFC822 string `xml:"observation_time_rfc822"`
 	Weather               string `xml:"weather"`
 	TempF                 string `xml:"temp_f"`
 	TempC                 string `xml:"temp_c"`
-	WaterTempF            string `xml:"water_temp_f"`
-	WaterTempC            string `xml:"water_temp_f"`
 	RelativeHumidity      string `xml:"relative_humidity"`
 	WindDir               string `xml:"wind_dir"`
 	WindDegrees           string `xml:"wind_degrees"`
 	WindMph               string `xml:"wind_mph"`
-	WindGustMph           string `xml:"wind_gust_mph"`
 	WindKt                string `xml:"wind_kt"`
+	WindGustMph           string `xml:"wind_gust_mph"`
 	WindGustKt            string `xml:"wind_gust_kt"`
 	PressureMb            string `xml:"pressure_mb"`
 	PressureIn            string `xml:"pressure_in"`
-	PressureTendencyMb    string `xml:"pressure_tendency_mb"`
-	PressureTendencyIn    string `xml:"pressure_tendency_in"`
 	DewpointF             string `xml:"dewpoint_f"`
 	DewpointC             string `xml:"dewpoint_c"`
 	HeatIndexF            string `xml:"heat_index_f"`
@@ -51,22 +81,49 @@ type Observation struct {
 	WindchillF            string `xml:"windchill_f"`
 	WindchillC            string `xml:"windchill_c"`
 	VisibilityMi          string `xml:"visibility_mi"`
-	WaveHeightM           string `xml:"wave_height_m"`
-	WaveHeightFt          string `xml:"wave_height_ft"`
-	DominantPeriodSec     string `xml:"dominat_period_sec"`
-	AveragePeriodSec      string `xml:"average_period_sec"`
-	MeanWaveDir           string `xml:"mean_wave_dir"`
-	MeanWaveDegrees       string `xml:"mean_wave_degrees"`
-	TideFt                string `xml:"tide_ft"`
-	Steepness             string `xml:"steepness"`
-	WaterColumnHeight     string `xml:"water_column_height"`
-	SurfHeightFt          string `xml:"surf_height_ft"`
-	SwellDir              string `xml:"swell_dir"`
-	SwellDegrees          string `xml:"swell_degrees"`
-	SwellPeriod           string `xml:"swell_period"`
+}
+
+// convert converts an unexported observation struct to an exported
+// observation struct
+func (o *observation) convert() (*Observation, error) {
+	ret := Observation{}
+	ret.Location = o.Location
+	ret.StationId = o.StationId
+	ret.Latitude = o.Latitude
+	ret.Longitude = o.Longitude
+	ret.Elevation = o.Elevation
+	// the name of the XML field implies that the time will be provided in
+	// RFC822 format, but it is actually RFC1123Z time as defined by the Go
+	// time package. 😞
+	time, err := time.Parse(observationTimeFmt, o.ObservationTimeRFC822)
+	if err != nil {
+		return nil, err
+	}
+	ret.Time = time
+	ret.Weather = o.Weather
+	ret.TempF = o.TempF
+	ret.TempC = o.TempC
+	ret.RelativeHumidity = o.RelativeHumidity
+	ret.WindDir = o.WindDir
+	ret.WindDegrees = o.WindDegrees
+	ret.WindMph = o.WindMph
+	ret.WindKt = o.WindKt
+	ret.WindGustMph = o.WindGustMph
+	ret.WindGustKt = o.WindGustKt
+	ret.PressureMb = o.PressureMb
+	ret.PressureIn = o.PressureIn
+	ret.DewpointF = o.DewpointF
+	ret.DewpointC = o.DewpointC
+	ret.HeatIndexF = o.HeatIndexF
+	ret.HeatIndexC = o.HeatIndexC
+	ret.WindchillF = o.WindchillF
+	ret.WindchillC = o.WindchillC
+	ret.VisibilityMi = o.VisibilityMi
+	return &ret, nil
 }
 
 // getCurrentObservationXML gets the most recent observation from a stationId
+// and returns a byte array containing XML
 func getCurrentObservationXML(httpClient *http.Client, stationId string) ([]byte, error) {
 	resp, err := httpClient.Get(fmt.Sprintf(currentObservationsURLFmt, stationId))
 	if err != nil {
@@ -79,26 +136,22 @@ func getCurrentObservationXML(httpClient *http.Client, stationId string) ([]byte
 	return ioutil.ReadAll(resp.Body)
 }
 
-// processObservationXML accepts an observation in XML and returns an
-// Observation sruct
+// processObservationXML accepts an observation in XML and returns a pointer to
+// an Observation struct
 func processObservationXML(observationXML []byte) (*Observation, error) {
-	obs := &Observation{}
-
-	if err := xml.Unmarshal(observationXML, obs); err != nil {
+	obs := &observation{}
+	// creating our own decoder is required since the character set isn't UTF-8
+	reader := bytes.NewReader(observationXML)
+	decoder := xml.NewDecoder(reader)
+	decoder.CharsetReader = charset.NewReaderLabel
+	if err := decoder.Decode(obs); err != nil {
 		return nil, err
 	}
-
-	var err error
-	obs.Time, err = time.Parse(time.RFC822, obs.observationTimeRFC822)
-	if err != nil {
-		return nil, err
-	}
-
-	return obs, nil
+	return obs.convert()
 }
 
 // getCurrentObservation gets the most recent observation from a stationId and
-// returns it
+// returns a pointer to an Observation struct
 func getCurrentObservation(httpClient *http.Client, stationId string) (*Observation, error) {
 	obsXML, err := getCurrentObservationXML(httpClient, stationId)
 	if err != nil {
--- observations_test.go
+++ observations_test.go
@@ -1,1 +1,134 @@
 package nws
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	httpmock "gopkg.in/jarcoal/httpmock.v1"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testObservation = `<?xml version="1.0" encoding="ISO-8859-1"?> 
+<?xml-stylesheet href="latest_ob.xsl" type="text/xsl"?>
+<current_observation version="1.0"
+	 xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+	 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	 xsi:noNamespaceSchemaLocation="http://www.weather.gov/view/current_observation.xsd">
+	<credit>NOAA's National Weather Service</credit>
+	<credit_URL>http://weather.gov/</credit_URL>
+	<image>
+		<url>http://weather.gov/images/xml_logo.gif</url>
+		<title>NOAA's National Weather Service</title>
+		<link>http://weather.gov</link>
+	</image>
+	<suggested_pickup>15 minutes after the hour</suggested_pickup>
+	<suggested_pickup_period>60</suggested_pickup_period>
+	<location>Fakeland, Fake International Airport, FK</location>
+	<station_id>KFAK</station_id>
+	<latitude>43.450101</latitude>
+	<longitude>-87.222019</longitude>
+	<elevation>0</elevation>
+	<observation_time>Last Updated on Feb 27 2017, 8:53 am PST</observation_time>
+    <observation_time_rfc822>Mon, 27 Feb 2017 08:53:00 -0800</observation_time_rfc822>
+	<weather>Overcast</weather>
+	<temperature_string>38.0 F (3.3 C)</temperature_string>
+	<temp_f>38.0</temp_f>
+	<temp_c>3.3</temp_c>
+	<relative_humidity>86</relative_humidity>
+	<wind_string>Southwest at 6.9 MPH (6 KT)</wind_string>
+	<wind_dir>Southwest</wind_dir>
+	<wind_degrees>230</wind_degrees>
+	<wind_mph>6.9</wind_mph>
+	<wind_kt>6</wind_kt>
+	<wind_gust_mph>200</wind_gust_mph>
+	<wind_gust_kt>173.8</wind_gust_kt>
+	<pressure_string>1009.9 mb</pressure_string>
+	<pressure_mb>1009.9</pressure_mb>
+	<pressure_in>29.82</pressure_in>
+	<dewpoint_string>34.0 F (1.1 C)</dewpoint_string>
+	<dewpoint_f>34.0</dewpoint_f>
+	<dewpoint_c>1.1</dewpoint_c>
+	<windchill_string>33 F (1 C)</windchill_string>
+    <windchill_f>33</windchill_f>
+    <windchill_c>1</windchill_c>
+    <heat_index_string>38.0 F (3.3 C)</heat_index_string>
+    <heat_index_f>38.0</heat_index_f>
+    <heat_index_c>3.3</heat_index_c>
+	<visibility_mi>10.00</visibility_mi>
+ 	<icon_url_base>http://forecast.weather.gov/images/wtf/small/</icon_url_base>
+	<two_day_history_url>http://www.weather.gov/data/obhistory/KPDX.html</two_day_history_url>
+	<icon_url_name>ovc.png</icon_url_name>
+	<ob_url>http://www.weather.gov/data/METAR/KPDX.1.txt</ob_url>
+	<disclaimer_url>http://weather.gov/disclaimer.html</disclaimer_url>
+	<copyright_url>http://weather.gov/disclaimer.html</copyright_url>
+	<privacy_policy_url>http://weather.gov/notice.html</privacy_policy_url>
+</current_observation>`
+)
+
+var (
+	parsedTestObservationTime, _ = time.Parse(time.RFC1123Z, "Mon, 27 Feb 2017 08:53:00 -0800")
+	parsedTestObservation        = Observation{
+		// SuggestedPickup:       "15 minutes after the hour",
+		// SuggestedPickupPeriod: "60",
+		Location:         "Fakeland, Fake International Airport, FK",
+		StationId:        "KFAK",
+		Latitude:         "43.450101",
+		Longitude:        "-87.222019",
+		Elevation:        "0",
+		Time:             parsedTestObservationTime,
+		Weather:          "Overcast",
+		TempF:            "38.0",
+		TempC:            "3.3",
+		RelativeHumidity: "86",
+		WindDir:          "Southwest",
+		WindDegrees:      "230",
+		WindMph:          "6.9",
+		WindKt:           "6",
+		WindGustMph:      "200",
+		WindGustKt:       "173.8",
+		PressureMb:       "1009.9",
+		PressureIn:       "29.82",
+		DewpointF:        "34.0",
+		DewpointC:        "1.1",
+		HeatIndexF:       "38.0",
+		HeatIndexC:       "3.3",
+		WindchillF:       "33",
+		WindchillC:       "1",
+		VisibilityMi:     "10.00",
+	}
+)
+
+func TestGetCurrentObservationXML(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	httpmock.RegisterResponder("GET",
+		"http://w1.weather.gov/xml/current_obs/KFAK.xml",
+		httpmock.NewStringResponder(200, testObservation))
+
+	c := &http.Client{}
+	obs, err := getCurrentObservationXML(c, "KFAK")
+	require.Nil(t, err)
+	require.Equal(t, obs, []byte(testObservation))
+}
+
+func TestProcessObservationXML(t *testing.T) {
+	obs, err := processObservationXML([]byte(testObservation))
+	require.Nil(t, err)
+	require.Equal(t, *obs, parsedTestObservation)
+}
+
+func TestGetCurrentObservation(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	httpmock.RegisterResponder("GET",
+		"http://w1.weather.gov/xml/current_obs/KFAK.xml",
+		httpmock.NewStringResponder(200, testObservation))
+
+	c := &http.Client{}
+	obs, err := getCurrentObservation(c, "KFAK")
+	require.Nil(t, err)
+	require.Equal(t, *obs, parsedTestObservation)
+}
