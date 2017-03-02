3a58ec580d95647ad339eac0d486e3efa6a0b605
Michael Camilleri <mike@mikecamilleri.com>
2017-03-02 09:06:47 -0800

    move HTTP mocks to sub-package

--- observations_test.go
+++ observations_test.go
@@ -7,65 +7,8 @@ import (
 
 	httpmock "gopkg.in/jarcoal/httpmock.v1"
 
+	"github.com/mikecamilleri/nws/mock"
 	"github.com/stretchr/testify/require"
-)
-
-const (
-	testObservation = `<?xml version="1.0" encoding="ISO-8859-1"?> 
-<?xml-stylesheet href="latest_ob.xsl" type="text/xsl"?>
-<current_observation version="1.0"
-	 xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-	 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	 xsi:noNamespaceSchemaLocation="http://www.weather.gov/view/current_observation.xsd">
-	<credit>NOAA's National Weather Service</credit>
-	<credit_URL>http://weather.gov/</credit_URL>
-	<image>
-		<url>http://weather.gov/images/xml_logo.gif</url>
-		<title>NOAA's National Weather Service</title>
-		<link>http://weather.gov</link>
-	</image>
-	<suggested_pickup>15 minutes after the hour</suggested_pickup>
-	<suggested_pickup_period>60</suggested_pickup_period>
-	<location>Fakeland, Fake International Airport, FK</location>
-	<station_id>KFAK</station_id>
-	<latitude>43.450101</latitude>
-	<longitude>-87.222019</longitude>
-	<elevation>0</elevation>
-	<observation_time>Last Updated on Feb 27 2017, 8:53 am PST</observation_time>
-    <observation_time_rfc822>Mon, 27 Feb 2017 08:53:00 -0800</observation_time_rfc822>
-	<weather>Overcast</weather>
-	<temperature_string>38.0 F (3.3 C)</temperature_string>
-	<temp_f>38.0</temp_f>
-	<temp_c>3.3</temp_c>
-	<relative_humidity>86</relative_humidity>
-	<wind_string>Southwest at 6.9 MPH (6 KT)</wind_string>
-	<wind_dir>Southwest</wind_dir>
-	<wind_degrees>230</wind_degrees>
-	<wind_mph>6.9</wind_mph>
-	<wind_kt>6</wind_kt>
-	<wind_gust_mph>200</wind_gust_mph>
-	<wind_gust_kt>173.8</wind_gust_kt>
-	<pressure_string>1009.9 mb</pressure_string>
-	<pressure_mb>1009.9</pressure_mb>
-	<pressure_in>29.82</pressure_in>
-	<dewpoint_string>34.0 F (1.1 C)</dewpoint_string>
-	<dewpoint_f>34.0</dewpoint_f>
-	<dewpoint_c>1.1</dewpoint_c>
-	<windchill_string>33 F (1 C)</windchill_string>
-    <windchill_f>33</windchill_f>
-    <windchill_c>1</windchill_c>
-    <heat_index_string>38.0 F (3.3 C)</heat_index_string>
-    <heat_index_f>38.0</heat_index_f>
-    <heat_index_c>3.3</heat_index_c>
-	<visibility_mi>10.00</visibility_mi>
- 	<icon_url_base>http://forecast.weather.gov/images/wtf/small/</icon_url_base>
-	<two_day_history_url>http://www.weather.gov/data/obhistory/KPDX.html</two_day_history_url>
-	<icon_url_name>ovc.png</icon_url_name>
-	<ob_url>http://www.weather.gov/data/METAR/KPDX.1.txt</ob_url>
-	<disclaimer_url>http://weather.gov/disclaimer.html</disclaimer_url>
-	<copyright_url>http://weather.gov/disclaimer.html</copyright_url>
-	<privacy_policy_url>http://weather.gov/notice.html</privacy_policy_url>
-</current_observation>`
 )
 
 var (
@@ -73,10 +16,10 @@ var (
 	parsedTestObservation        = Observation{
 		// SuggestedPickup:       "15 minutes after the hour",
 		// SuggestedPickupPeriod: "60",
-		Location:         "Fakeland, Fake International Airport, FK",
-		StationId:        "KFAK",
-		Latitude:         "43.450101",
-		Longitude:        "-87.222019",
+		Location:         "Portland, Portland International Airport, OR",
+		StationId:        "KPDX",
+		Latitude:         "45.59578",
+		Longitude:        "-122.60917",
 		Elevation:        "0",
 		Time:             parsedTestObservationTime,
 		Weather:          "Overcast",
@@ -105,13 +48,10 @@ var (
 func TestGetCurrentObservation(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
-	httpmock.RegisterResponder("GET",
-		"http://w1.weather.gov/xml/current_obs/KFAK.xml",
-		httpmock.NewStringResponder(200, testObservation),
-	)
+	mock.RegisterResponders()
 
 	c := &http.Client{}
-	obs, err := getCurrentObservation(c, "KFAK")
+	obs, err := getCurrentObservation(c, "KPDX")
 
 	require.Nil(t, err)
 	require.Equal(t, *obs, parsedTestObservation)
